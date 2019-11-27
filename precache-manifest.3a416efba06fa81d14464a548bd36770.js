f1946b5299e287052d3875d106f0cbc3c7eb6a7d
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-11-26 22:39:39 -0300

    first commit

---
+++ precache-manifest.3a416efba06fa81d14464a548bd36770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ba1d3dacd3901171ad28fd459ed069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa09b9721d8c6af882d",
+    "url": "/static/css/main.44055736.chunk.css"
+  },
+  {
+    "revision": "833b731c5047357d22d9",
+    "url": "/static/js/2.6f576ab2.chunk.js"
+  },
+  {
+    "revision": "9aa09b9721d8c6af882d",
+    "url": "/static/js/main.f7fdf34b.chunk.js"
+  },
+  {
+    "revision": "13d196ae8126bf363e10",
+    "url": "/static/js/runtime-main.0c3a017f.js"
+  },
+  {
+    "revision": "ee9c1c4bc9843e835dec186425359846",
+    "url": "/static/media/loading.ee9c1c4b.gif"
+  },
+  {
+    "revision": "0c6e02ca76b64b2dd054c9c9e31e1353",
+    "url": "/static/media/olho.0c6e02ca.png"
+  }
+]);
